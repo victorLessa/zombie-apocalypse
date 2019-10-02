1981bc0b2e0ca0685d7da73aa526b4b27509ff11
Victor Lessa <victordsgnr@gmail.com>
2019-10-02 18:10:00 -0300

    infection_indicator table creation

---
+++ src/migrations/20191002204513-infection-indicator.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const tableName = 'infection_indicator'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable(tableName, 
+      { 
+        id: {
+          type: Sequelize.INTEGER.UNSIGNED,
+          autoIncrement: true,
+          primaryKey: true,
+          allowNull: false,
+          unique: true
+        },
+        survivor_id: {
+          type: Sequelize.INTEGER.UNSIGNED,
+          allowNull: true,
+          references: {
+            model: 'survivors',
+            key: 'id'
+          },
+          onDelete: 'CASCADE'
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+        },
+        updated_at: {
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+        }
+      });
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+    */
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable(tableName);
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+    */
+  }
+};
