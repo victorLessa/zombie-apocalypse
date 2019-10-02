55de4fd226916e21e0004bc8bba6bc652beb3f43
Victor Lessa <victordsgnr@gmail.com>
2019-10-02 15:31:40 -0300

    Controller Survivors

---
+++ src/controller/SurvivorsController.js
@@ -0,0 +1,22 @@
+// indes, show, store, destroy, update
+module.exports = function (SurvivorService) {
+  return {
+    async index (req, res, next) {
+      try {
+        res.status(200).send('Hello World')
+      } catch (error) {
+        res.status(500).send(error)
+        next();
+      }
+    },
+    async store (req, res, next) {
+      try {
+        let result = await SurvivorService.create(req.body)
+        res.status(200).send(result)
+      } catch (error) {
+        res.status(500).send(error)
+        next(error)
+      }
+    }
+  }
+}
