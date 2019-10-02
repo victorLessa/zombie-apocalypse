217cf4955407a4c3250e35a41a1be515148d2b5d
Victor Lessa <victordsgnr@gmail.com>
2019-10-02 16:27:59 -0300

    Checking if Survivor Already in Database

--- src/service/Survivors.js
+++ src/service/Survivors.js
@@ -9,24 +9,31 @@ class Survivors {
     return result
   }
   async create ({name, age, sex, last_place, ...items}) {
-    let result = await this.db.transaction(async trx => {
-      await this.db('survivors')
-        .transacting(trx)
-        .insert({ name, age, sex, last_place })
-        .then(async resp => {
-          for (let i = 0; i < items.items.length; i++) {
-            items.items[i].survivor_id = resp[0]
-            await this.db('inventory')
-              .transacting(trx)
-              .insert({
-                survivor_id: items.items[i].survivor_id,
-                item_id: items.items[i].item_id,
-                quantity: items.items[i].quantity
-              })
-          }
-        }).catch(trx.rollback)
-    })
-    return result
+    let hasSurvivors = await this.db('survivors').where({ name, age, sex })
+    let promise;
+    if (hasSurvivors.length < 1) {
+      await this.db.transaction(async trx => {
+        await this.db('survivors')
+          .transacting(trx)
+          .insert({ name, age, sex, last_place })
+          .then(async resp => {
+            for (let i = 0; i < items.items.length; i++) {
+              items.items[i].survivor_id = resp[0]
+              await this.db('inventory')
+                .transacting(trx)
+                .insert({
+                  survivor_id: items.items[i].survivor_id,
+                  item_id: items.items[i].item_id,
+                  quantity: items.items[i].quantity
+                })
+            }
+          }).catch(trx.rollback)
+          promise = await this.db('survivors').select('name', 'age').first()
+      })
+    } else {
+      throw { message:'User is already in the database', status: 500 };
+    }
+    return promise
   }
 }
 
