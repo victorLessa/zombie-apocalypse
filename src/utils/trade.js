8ff08e20c4729dba481da3ae4bd035e8c8353c41
Victor Lessa <victordsgnr@gmail.com>
2019-10-02 23:11:46 -0300

    utils responsible for calculating whether the transaction is possible

---
+++ src/utils/trade.js
@@ -0,0 +1,22 @@
+'use strict'
+
+class Trade {
+  contructor() {
+
+  }
+  isPossible ({ currentSurvivor, otherSurvivor }) {
+    let currentSruvivorPoints = 
+      currentSurvivor.quantity * 
+      currentSurvivor.points
+    let otherSurvivorPoints = 
+      otherSurvivor.quantity * 
+      otherSurvivor.points
+    if (currentSruvivorPoints === otherSurvivorPoints) {
+      return true
+    } else {
+      return false
+    }
+  }
+}
+
+module.exports = new Trade();
