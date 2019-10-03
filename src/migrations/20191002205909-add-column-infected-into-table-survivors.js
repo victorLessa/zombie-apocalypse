9edb08f790c0e7ae71f52cee5a9e7e1d3a109983
Victor Lessa <victordsgnr@gmail.com>
2019-10-03 09:43:44 -0300

    fixing error in migration where down method contained removal error

--- src/migrations/20191002205909-add-column-infected-into-table-survivors.js
+++ src/migrations/20191002205909-add-column-infected-into-table-survivors.js
@@ -10,12 +10,8 @@ module.exports = {
       allowNull: true,
       defaultValue: false,
   }),
-  down: (queryInterface, Sequelize) => {
-    queryInterface.addColumn(tableName, columnName, {
-      type: Sequelize.BOOLEAN,
-      allowNull: true,
-      defaultValue: false
-    })
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn(tableName, columnName)
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
