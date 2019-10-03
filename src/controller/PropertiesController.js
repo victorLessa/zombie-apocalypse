90d406b9766159334c0f8702ce1bb36d88c337a7
Victor Lessa <victordsgnr@gmail.com>
2019-10-03 16:33:01 -0300

    control to return items from all survivors and one specific survivor

--- src/controller/PropertiesController.js
+++ src/controller/PropertiesController.js
@@ -1,11 +1,30 @@
-const TradeController = function (TradeService) {
-  this.TradeService = TradeService;
+const TradeController = function (PropertiesService) {
+  this.PropertiesService = PropertiesService;
   return {
+    async index(req, res, next) {
+      try {
+        let result = await this.PropertiesService.index(req.params);
+        res.send(result)
+      } catch (error) {
+        res.status(error.error || 500).send(error)
+        next(error)
+      }
+    },
+    async show (req, res, next) {
+      try {
+        let result = await this.PropertiesService.show();
+        console.log(result)
+        res.send(result)
+      } catch(error) {
+        res.send(error)
+        next(error)
+      }
+    },
     async tradeItems(req, res, next) {
       try {
         let body = req.body;
         body.from.survivor_id = parseInt(req.params.id);
-        let result = await this.TradeService.tradeItems(body);
+        let result = await this.PropertiesService.tradeItems(body);
         res.status(200).send(result);
       } catch (error) {
         res.status(error.status || 500).send(error)
