5d3fb125f92dadc9a7a0af6408ecdea5f0b68f31
Victor Lessa <victordsgnr@gmail.com>
2019-10-04 11:42:39 -0300

    mocha test file

--- test/survivors.test.js
+++ test/survivors.test.js
@@ -3,52 +3,182 @@ const server = require('../src/app');
 
 const chaitHttp = require('chai-http');
 const should = chai.should();
-
 chai.use(chaitHttp);
 
-describe('Survivors', function() {
-  
+describe('Automated test', function () {
   let id;
-  afterEach(function (next) {
-    next();
-  })
-  it('Register Survivors', function (done) {
+  let anotherSurvivorId;
+  
+  describe('Survivors', function () {
 
-    const body = {
-      "name": "Codeminer",
-      "age": 21,
-      "sex": "Masculino",
-      "last_place": null,
-      "items": [
-        {"item_id": 1, "quantity": 1},
-        {"item_id": 2, "quantity": 5},
-        {"item_id": 3, "quantity": 3},
-        {"item_id": 4, "quantity": 4}
-      ]
-    }
-    chai.request(server)
-      .post('/api/survivors/register')
-      .send(body)
-      .end(function (err, res) {
-        if (err) return done(err);
-        res.should.have.status(200);
-        res.body.should.have.property('id');
-        res.body.should.have.property('name');
-        res.body.should.have.property('age');
-        res.body.name.should.be.equal('Codeminer');
+    it('Register Survivors', function (done) {
 
-        id = res.body.id;
-        done();
-      });
+      const body = {
+        "name": "Codeminer",
+        "age": 21,
+        "sex": "Masculino",
+        "last_place": "-22.8945014,-43.1185222",
+        "items": [
+          { "item_id": 1, "quantity": 1 },
+          { "item_id": 2, "quantity": 5 },
+          { "item_id": 3, "quantity": 3 },
+          { "item_id": 4, "quantity": 4 }
+        ]
+      }
+      chai.request(server)
+        .post('/api/survivors/register')
+        .send(body)
+        .end(function (err, res) {
+          if (err) return done(err);
+          res.should.have.status(200);
+          res.body.should.have.property('id');
+          res.body.should.have.property('name');
+          res.body.should.have.property('age');
+          res.body.name.should.be.equal('Codeminer');
+
+          id = res.body.id;
+          done();
+        });
+    });
+
+    it('Return survivor by id', function (done) {
+      chai.request(server)
+        .get(`/api/survivors/${id}`)
+        .end(function (err, res) {
+          res.should.have.status(200)
+          res.body.should.have.property('result')
+          done();
+        })
+    });
+
+    it('Return all survivor', function (done) {
+      chai.request(server)
+        .get('/api/survivors')
+        .end(function (err, res) {
+          res.should.have.status(200);
+          res.body.should.have.property('result');
+          done();
+        })
+    });
+
+    it('Update survivor locale', function (done) {
+
+      const place = {
+        "last_place": "-22.8945014,-43.1185222"
+      }
+
+      chai.request(server)
+        .patch(`/api/survivors/${id}/place`)
+        .send(place)
+        .end(function (err, res) {
+          let pointPlace = place.last_place.split(',')
+          res.should.have.status(200);
+          res.body.should.have.property('result');
+          res.body.result.last_place.x.should.be.equal(parseFloat(pointPlace[0]));
+          res.body.result.last_place.y.should.be.equal(parseFloat(pointPlace[1]));
+          done();
+        });
+    });
+
+    it('Reports if one survivor is infected', function (done) {
+      chai.request(server)
+        .patch(`/api/survivors/report_infection/${id}`)
+        .end(function (err, res) {
+          res.should.have.status(200);
+          res.body.should.have.property('message');
+          res.body.message.should.be.equal('Successful alert');
+          done();
+        });
+    });
   });
 
-  it('Return survivor by id', function (done) {
-    chai.request(server)
-      .get(`/api/survivors/${id}`)
-      .end(function (err, res) {
-        res.should.have.status(200)
-        res.body.should.have.property('result')
-        done();
-      })
+  describe('Reports', function () {
+
+    it('Return statistics about zombie infection', function (done) {
+      chai.request(server)
+        .get(`/api/reports`)
+        .end(function (err, res) {
+          res.should.have.status(200)
+        });
+      done();
+    });
+
+  });
+
+  describe('Properties', function () {
+
+    it('Fetches all items of the survivor', function (done) {
+      chai.request(server)
+        .get(`/api/properties/${id}`)
+        .end(function (err, res) {
+          res.should.have.status(200)
+          done();
+        });
+    });
+
+    it('Search all items from all survivors', function (done) {
+      chai.request(server)
+        .get(`/api/properties`)
+        .end(function (err, res) {
+          res.should.have.status(200);
+          res.body.should.have.property('result');
+          done();
+        });
+    });
+
+    it('Perform a new survivor record', function (done) {
+
+      const anotherBody = {
+        "name": "Codeminer42",
+        "age": 21,
+        "sex": "Masculino",
+        "last_place": "-22.8945014,-43.1185222",
+        "items": [
+          { "item_id": 1, "quantity": 1 },
+          { "item_id": 2, "quantity": 5 },
+          { "item_id": 3, "quantity": 3 },
+          { "item_id": 4, "quantity": 4 }
+        ]
+      }
+      chai.request(server)
+        .post('/api/survivors/register')
+        .send(anotherBody)
+        .end(function (err, res) {
+          if (err) return done(err);
+          res.should.have.status(200);
+          res.body.should.have.property('id');
+          res.body.should.have.property('name');
+          res.body.should.have.property('age');
+          res.body.name.should.be.equal('Codeminer42');
+
+          anotherSurvivorId = res.body.id;
+          done();
+        });
+
+    });
+    it('Make a trade transaction between survivors', function (done) {
+
+      const trade = {
+        "from": {
+          "item_id": 4,
+          "quantity": 4
+        },
+        "per": {
+          "survivor_id": anotherSurvivorId,
+          "item_id": 1,
+          "quantity": 1
+        }
+      }
+
+      chai.request(server)
+        .post(`/api/properties/${id}`)
+        .send(trade)
+        .end(function (err, res) {
+          if (err) done(err)
+          res.should.have.status(200);
+          done();
+        });
+    });
+
   });
 });
